912d8ac86a9eb3cb2ff8986cb5204347eac0faf2
Alams <alamshahi.mahsa@gmail.com>
2024-09-07 19:42:10 +0330

    API Key changed.

--- app/src/main/java/org/nextoptech/news/common/AppConstant.kt
+++ app/src/main/java/org/nextoptech/news/common/AppConstant.kt
@@ -4,9 +4,9 @@ object AppConstant {
 
     const val API_KEY = "d138b50732644689bca64a1b987eea5d"
     const val BASE_URL = "https://newsapi.org/v2/"
-    const val DB_NAME ="newsDatabase"
+    const val DB_NAME = "newsDatabase"
 
     const val BULLET = "\u2022"
     const val NEWS_DETAILS_NAVIGATION_ARGUMENT_KEY = "newsDetailsArgKey"
-    const val PAGE_SIZE = 3
+    const val PAGE_SIZE = 5
 }
